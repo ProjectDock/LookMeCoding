c5f18459e32dd735718ed15141cdccde29ffb414
Istvan Acs <istvan.acs89@gmail.com>
2020-10-10 10:02:28 +0100

    Add myself as a contributor for hactoberfest

--- src/CONTRIBUTE_HERE/content.js
+++ src/CONTRIBUTE_HERE/content.js
@@ -1,29 +1,32 @@
-
 const cards = [
+  /** ======== Add your info below -- Do not modify anything outside this box ======== **/
 
-/** ======== Add your info below -- Do not modify anything outside this box ======== **/
+  {
+    name: 'Project Dock',
+    title: 'JS Developer',
+    image: 'PD/pd.png',
+    github: 'https://github.com/ProjectDock',
+  },
+  {
+    name: 'Tamas Kn',
+    title: 'Full Stack Developer',
+    image: 'TamasKn/TamasKn.png',
+    github: 'https://github.com/TamasKn',
+  },
+  {
+    name: 'Saurabh kamble',
+    title: 'Full Stack Developer',
+    image: 'saurabh2836/game_of_throne_theme.png',
+    github: 'https://github.com/saurabh2836',
+  },
+  {
+    name: 'Istvan Acs',
+    title: 'Software Engineer',
+    image: 'IstvanAcs/IstvanAcs.jpg',
+    github: 'https://github.com/St3ve89',
+  },
 
-    {
-        name: "Project Dock",
-        title: "JS Developer",
-        image: "PD/pd.png",
-        github: "https://github.com/ProjectDock"
-    },
-    {
-        name: "Tamas Kn",
-        title: "Full Stack Developer",
-        image: "TamasKn/TamasKn.png",
-        github: "https://github.com/TamasKn"
-    },
-    {
-        name: "Saurabh kamble",
-        title: "Full Stack Developer",
-        image: "saurabh2836/game_of_throne_theme.png",
-        github: "https://github.com/saurabh2836"
-    },
+  /** ======== Place your infos above ======== **/
+];
 
-/** ======== Place your infos above ======== **/
-
-]
-
-export default cards
+export default cards;
